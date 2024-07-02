cd1cb5e75075084aac68ebeef1cb3801a7de41b8
NihalRawat <127190177+NihalRawat@users.noreply.github.com>
2024-07-03 00:00:41 +0530

    updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   loginForm:FormGroup | undefined
   constructor(
-    private service:AuthService,
+    private authService:AuthService,
     private fb:FormBuilder
   ) { }
 
@@ -25,7 +25,10 @@ export class LoginComponent implements OnInit {
   }
   login(){
     console.log(this.loginForm.value);
-    this.service.login(this.loginForm.value).subscribe((response)=>{
+    this.authService.login(
+    this.loginForm.get(['email'])!.value,
+    this.loginForm.get(['password'])!.value,
+    ).subscribe((response)=>{
       console.log(response);
     })
   }
