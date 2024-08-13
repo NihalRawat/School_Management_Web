d1eeb4bd170df52f1f2a24581d84f7efaf631c4d
NihalRawat <127190177+NihalRawat@users.noreply.github.com>
2024-08-14 00:03:59 +0530

    push

---
+++ src/app/modules/student/student-comp/get-all-leaves/get-all-leaves.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../student-servie/student.service';
+
+@Component({
+  selector: 'app-get-all-leaves',
+  templateUrl: './get-all-leaves.component.html',
+  styleUrls: ['./get-all-leaves.component.css']
+})
+export class GetAllLeavesComponent implements OnInit {
+
+    isSpinning=false;
+    leaves:any;
+
+  constructor(private studentService:StudentService) { }
+
+  ngOnInit(): void {
+    this.getAllLeaves();
+  }
+
+  getAllLeaves(){
+    this.isSpinning=true;
+    this.studentService.getAllAppliedLeaves().subscribe(
+      (res)=>{
+        console.log(res);
+        this.isSpinning=false;
+        this.leaves=res;
+      }
+    )
+  }
+}
