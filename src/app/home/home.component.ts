ee53cfb4185e1c10814088d53b79381ecea35cfe
NihalRawat <127190177+NihalRawat@users.noreply.github.com>
2024-09-12 00:00:06 +0530

    fix

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private service:HomeService) { }
+
+  ngOnInit(): void {
+    this.getAllTeacher();
+  }
+  getAllTeacher(){
+    this.service.getAllTeacher().subscribe(
+      (res)=>{
+        console.log(res);
+      }
+    )
+  }
+
+}
---
+++ src/app/home/home.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const keys='http://localhost:8083/';
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class HomeService {
+  
+  constructor(private http:HttpClient) { }
+  getAllTeacher():Observable<any>{
+      return this.http.get(keys+"teachers");
+  }
+
+
+}
