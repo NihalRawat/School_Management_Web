77acc15e857fd9c2351623a81a6022bfae3b10b2
NihalRawat <127190177+NihalRawat@users.noreply.github.com>
2024-09-29 22:57:48 +0530

    caroule not fixed

---
+++ src/app/modules/shared/empty-notification/loginCarousel.ts
@@ -0,0 +1,20 @@
+export const loginCarousel=[
+
+    {
+        image:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp",
+        path:"/login/page1"
+    },{
+        image:"https://cdn.dribbble.com/userupload/6836588/file/original-b80f6b55b6656398e22eb5111c501e10.png?resize=1200x900",
+        path:"/login/page2"
+    },{
+        image:"https://imgs.search.brave.com/mwjTZ5jkyW9FmsljUZLlwZrNRj2ID4wr-e6CQ5rIHTM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTkz/OTkyOTA4My9waG90/by9jeWJlci1zZWN1/cml0eS1jb25jZXB0/LXVzZXItZW50ZXJz/LXBhc3N3b3JkLXRv/LWxvZ2luLXRvLXBy/aXZhY3ktYW5kLXBy/b3RlY3QtcGVyc29u/YWwtZGF0YS53ZWJw/P2E9MSZiPTEmcz02/MTJ4NjEyJnc9MCZr/PTIwJmM9RlBhMXV3/T1RTQzFkTU9zRTVr/SHItVHFjZmg1ekhE/Tkd5Z2ZLLXpvUG10/Zz0",
+        path:"/login/page3"
+    },
+    {
+        image:"https://imgs.search.brave.com/zyGbTzgYybXNIdAeoWv9nR243ZZ-Jo4LpHOghjqBjlc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/Njk4ODgwOS9waG90/by9zZWN1cml0eS1w/YXNzd29yZC1sb2dp/bi1vbmxpbmUtY29u/Y2VwdC1oYW5kcy10/eXBpbmctYW5kLWVu/dGVyaW5nLXVzZXJu/YW1lLWFuZC1wYXNz/d29yZC1vZi53ZWJw/P2E9MSZiPTEmcz02/MTJ4NjEyJnc9MCZr/PTIwJmM9QlRzcHdq/cjJnZTJxTUxjVXlk/RGItT2VaMDFkWGFM/NnVHSGdHakx0ekt6/Zz0",
+        path:"/login/page4"
+    },{
+        image:"https://t3.ftcdn.net/jpg/08/11/54/20/240_F_811542085_RDybaqIpDicVvxT9uxjUE2C3BrvKgOFX.jpg",
+        path:"/login/page5"
+    }
+]
---
+++ src/app/modules/shared/loginpage-carousel/loginpage-carousel.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { loginCarousel } from '../empty-notification/loginCarousel';
+
+@Component({
+  selector: 'app-loginpage-carousel',
+  templateUrl: './loginpage-carousel.component.html',
+  styleUrls: ['./loginpage-carousel.component.css']
+})
+export class LoginpageCarouselComponent implements OnInit {
+  carouselData:any;
+  currentSlide=0;
+  interval:any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.carouselData=loginCarousel;
+    this.autoPlay()
+  }
+  autoPlay(){
+    setInterval(()=>{
+       this.nextSlide();
+     },2000)
+     }
+ 
+     nextSlide(){
+       this.currentSlide=(this.currentSlide+1) % this.carouselData.length
+       //when ever the carousel data reached at the last index then it will set to the first index.
+     }
+     
+}
